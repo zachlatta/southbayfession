2f2dd27849247bbd93b17aadcfc9fc4591cea583
Zach Latta <zach@zachlatta.com>
2014-03-05 05:08:27 -0800

    Don't print .school to the console in SchoolController.

--- public/js/school/school-controller.js
+++ public/js/school/school-controller.js
@@ -4,7 +4,6 @@ angular.module('southbayfession')
   .controller('SchoolController', ['$scope', '$modal', '$routeParams', 'School',
     function ($scope, $modal, $routeParams, School) {
       $scope.school = School.get({id: $routeParams.id});
-      console.log($scope.school);
       $scope.twitterUrl = function (tweet) {
         return "https://twitter.com/southbayfession/status/" + tweet.twitterId;
       };
