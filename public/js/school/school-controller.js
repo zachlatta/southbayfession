5c4a7901fc04425d6eb1a474db504ca7d55e676c
Zach Latta <zach@zachlatta.com>
2014-03-05 04:50:48 -0800

    Display a link to the tweet on Twitter alongside the link's text.

--- public/js/school/school-controller.js
+++ public/js/school/school-controller.js
@@ -4,4 +4,8 @@ angular.module('southbayfession')
   .controller('SchoolController', ['$scope', '$modal', '$routeParams', 'School',
     function ($scope, $modal, $routeParams, School) {
       $scope.school = School.get({id: $routeParams.id});
+      console.log($scope.school);
+      $scope.twitterUrl = function (tweet) {
+        return "https://twitter.com/southbayfession/status/" + tweet.twitterId;
+      };
     }]);
