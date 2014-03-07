0d2f6bdbe484205a04c2544e74ec29e548396a43
Zach Latta <zach@zachlatta.com>
2014-03-06 19:15:20 -0800

    Add link to Twitter in navbar.

--- public/js/nav/nav-controller.js
+++ public/js/nav/nav-controller.js
@@ -1,4 +1,5 @@
 angular.module('southbayfession')
   .controller('NavController', ['$scope', 'School', function ($scope, School) {
     $scope.schools = School.query();
+    $scope.twitter = 'https://twitter.com/southbayfession';
   }]);
